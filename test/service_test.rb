fd99bc1b0014d6e1bee5357acbf15a32140c5bed
Mario Izquierdo <marioizquierdo@twitch.tv>
2018-02-16 16:43:58 -0800

    Change service_test example to use Haberdasher instead of ServiceFoo

--- test/service_test.rb
+++ test/service_test.rb
@@ -5,36 +5,46 @@ require 'google/protobuf'
 require_relative '../lib/twirp'
 
 # Protobuf messages.
-# This is what the protoc code generator would produce.
+# An example of the result of the protoc ruby code generator.
 Google::Protobuf::DescriptorPool.generated_pool.build do
-  add_message "foopkg.DoFooRequest" do
-    optional :foo, :string, 1
+  add_message "example.Size" do
+    optional :inches, :int32, 1
   end
-  add_message "foopkg.DoFooResponse" do
-    optional :bar, :string, 1
+  add_message "example.Hat" do
+    optional :inches, :int32, 1
+    optional :color, :string, 2
+    optional :name, :string, 3
   end
 end
-module FooPkg
-  DoFooRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("foopkg.DoFooRequest").msgclass
-  DoFooResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("foopkg.DoFooResponse").msgclass
+
+module Example
+  Size = Google::Protobuf::DescriptorPool.generated_pool.lookup("example.Size").msgclass
+  Hat = Google::Protobuf::DescriptorPool.generated_pool.lookup("example.Hat").msgclass
 end
 
 # Twirp Service.
-# This is wha the twirp_ruby protoc plugin code generator would produce.
-module FooPkg
-  class FooService < Twirp::Service
-    package "foopkg"
-    service "FooService"
+# An example of the result of the protoc twirp_ruby plugin code generator.
+module Example
+  class Haberdasher < Twirp::Service
+    package "example"
+    service "Haberdasher"
 
-    rpc "DoFoo", DoFooRequest, DoFooResponse, handler_method: :do_foo
+    rpc "MakeHat", Size, Hat, handler_method: :make_hat
   end
 end
 
 # Example service handler.
 # This would be provided by the developer as implementation for the service.
-class FooHandler
-  def do_foo(req)
-    {bar: "Hello #{req.foo}"}
+class HaberdasherHandler
+  def make_hat(size)
+    if size.inches < 0
+      return Twirp::Error.new(:invalid_argument, "I can't make a hat that small!", argument: "inches")
+    end
+    {
+      inches: size.inches,
+      color: "white",
+      name:  "derby",
+    }
   end
 end
 
@@ -46,18 +56,18 @@ class ServiceTest < Minitest::Test
 
   def test_rpc_methods
     # make sure that rpcs have been properly setup by the `rpc` DSL constructor
-    assert_equal 1, FooPkg::FooService.rpcs.size
+    assert_equal 1, Example::Haberdasher.rpcs.size
     assert_equal({
-      request_class: FooPkg::DoFooRequest,
-      response_class: FooPkg::DoFooResponse,
-      handler_method: :do_foo,
-    }, FooPkg::FooService.rpcs["DoFoo"])
+      request_class: Example::Size,
+      response_class: Example::Hat,
+      handler_method: :make_hat,
+    }, Example::Haberdasher.rpcs["MakeHat"])
   end
 
   def test_package_service_getters
-    assert_equal "foopkg", FooPkg::FooService.package_name
-    assert_equal "FooService", FooPkg::FooService.service_name
-    assert_equal "foopkg.FooService", FooPkg::FooService.path_prefix
+    assert_equal "example", Example::Haberdasher.package_name
+    assert_equal "Haberdasher", Example::Haberdasher.service_name
+    assert_equal "example.Haberdasher", Example::Haberdasher.path_prefix
 
     assert_equal "", EmptyService.package_name # defaults to empty string
     assert_equal "EmptyService", EmptyService.service_name # defaults to class name
@@ -66,7 +76,7 @@ class ServiceTest < Minitest::Test
   
   def test_initialize_service
     # simple initialization
-    svc = FooPkg::FooService.new(FooHandler.new)
+    svc = Example::Haberdasher.new(HaberdasherHandler.new)
     assert svc.respond_to?(:call) # so it is a Proc that can be used as Rack middleware
 
     empty_svc = EmptyService.new(nil) # an empty service does not need a handler
@@ -74,32 +84,32 @@ class ServiceTest < Minitest::Test
   end
 
   def test_path_prefix
-    svc = FooPkg::FooService.new(FooHandler.new)
-    assert_equal "foopkg.FooService", svc.path_prefix
+    svc = Example::Haberdasher.new(HaberdasherHandler.new)
+    assert_equal "example.Haberdasher", svc.path_prefix
   end
 
   def test_initialize_fails_on_invalid_handlers
     assert_raises ArgumentError do 
-      FooPkg::FooService.new() # handler is mandatory
+      Example::Haberdasher.new() # handler is mandatory
     end
 
     # verify that handler implements required methods
     err = assert_raises ArgumentError do 
-      FooPkg::FooService.new("fake handler")
+      Example::Haberdasher.new("fake handler")
     end
-    assert_equal "Handler must respond to .do_foo(req) in order to handle the message DoFoo.", err.message
+    assert_equal "Handler must respond to .make_hat(req) in order to handle the message MakeHat.", err.message
   end
 
   def test_successful_simple_request
-    env = Rack::MockRequest.env_for("/twirp/foopkg.FooService/DoFoo", method: "POST", 
-      input: '{"foo": "World"}', "CONTENT_TYPE" => "application/json")
+    env = Rack::MockRequest.env_for("/twirp/example.Haberdasher/MakeHat", method: "POST", 
+      input: '{"inches": 10}', "CONTENT_TYPE" => "application/json")
 
-    svc = FooPkg::FooService.new(FooHandler.new)
+    svc = Example::Haberdasher.new(HaberdasherHandler.new)
     status, headers, body = svc.call(env)
 
     assert_equal 200, status
     assert_equal 'application/json', headers['Content-Type']
-    assert_equal body[0], '{"bar":"Hello World"}'
+    assert_equal body[0], '{"inches":10,"color":"white","name":"derby"}'
   end
 
 end
