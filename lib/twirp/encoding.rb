9f82040807832b34093f02db449a3d5fa5ef92d4
Mario Izquierdo <tothemario@gmail.com>
2020-11-16 12:21:44 -0800

    Use emit_defaults: true by deafult with JSON encoding, to be in sync with the Go implementation

--- lib/twirp/encoding.rb
+++ lib/twirp/encoding.rb
@@ -36,8 +36,7 @@ module Twirp
 
       def encode(msg_obj, msg_class, content_type)
         case content_type
-        when JSON then msg_class.encode_json(msg_obj, emit_defaults: false)
-        when JSON_STRICT then msg_class.encode_json(msg_obj, emit_defaults: true)
+        when JSON, JSON_STRICT then msg_class.encode_json(msg_obj, emit_defaults: true)
         when PROTO then msg_class.encode(msg_obj)
         else raise ArgumentError.new("Invalid content_type")
         end
