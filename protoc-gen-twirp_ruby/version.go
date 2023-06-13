23f093d768c79bdea09c5bba8326f8570022fd00
Arthur Nogueira Neves <arthurnn@gmail.com>
2023-06-12 21:27:07 -0400

    v1.11.0 go release

--- protoc-gen-twirp_ruby/version.go
+++ protoc-gen-twirp_ruby/version.go
@@ -15,4 +15,4 @@ package main
 
 // Version is the same version in the ruby gem: lib/twirp/version.rb
 // It helps identify what version of the code generator was used.
-const Version = "1.10.0"
+const Version = "1.11.0"
